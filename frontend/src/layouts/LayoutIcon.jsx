9994b7567832c3922f4242e64c3469dc8a3ca3ef
PhucLee14 <lhphucth14@gmail.com>
2025-03-14 00:03:14 +0700

    update icon layout

--- frontend/src/layouts/LayoutIcon.jsx
+++ frontend/src/layouts/LayoutIcon.jsx
@@ -1,61 +1,184 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+    Box,
+    ClickAwayListener,
+    IconButton,
+    Menu,
+    MenuItem,
+    TextField,
+    Paper,
+    Typography,
+    Divider,
+} from "@mui/material";
 import PageLink from "../components/PageLink";
-import { Link } from "react-router-dom";
 import SearchPopup from "../components/Popup/SearchPopup";
+import { getUser } from "../services/userService";
+import { logout } from "../services/authService";
+import CreatePostModal from "../components/CreatePostModal";
 
-function LayoutIcon({ children }) {
+function DefaultLayout({ children }) {
+    const nav = useNavigate();
     const [display, setDisplay] = useState(false);
+    const [user, setUser] = useState(null);
+    const [createModal, setCreateModal] = useState(false);
+    const [menuAnchor, setMenuAnchor] = useState(null);
+    const [openSearch, setOpenSearch] = useState(false);
+    const [navWidth, setNavWidth] = useState(250);
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const data = await getUser();
+                setUser(data);
+            } catch (error) {
+                console.error("Failed to fetch user:", error);
+            }
+        };
+
+        fetchUser();
+    }, []);
+
+    const handleLogout = async () => {
+        try {
+            await logout();
+            localStorage.removeItem("user");
+            setUser(null);
+            nav("/login");
+        } catch (error) {
+            console.log("error: ", error);
+        }
+    };
+
+    const handleSearch = () => {
+        setDisplay(!display);
+    };
+
+    const handleMenuOpen = (event) => {
+        setMenuAnchor(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setMenuAnchor(null);
+    };
+
+    const handleSearchOpen = () => {
+        setNavWidth(72);
+        setOpenSearch(true);
+    };
+    const handleSearchClose = () => {
+        setNavWidth(250);
+        setOpenSearch(false);
+    };
+
     return (
-        <div className="flex">
-            <div className="w-18 h-screen border-r border-gray-300 flex flex-col">
-                <div className="w-full py-6 flex justify-center h-24">
-                    <Link to="/" className="text-2xl block px-4">
-                        <i class="fa-brands fa-instagram"></i>
-                    </Link>
-                </div>
-                <PageLink
-                    icon={<i className="fa-light fa-house"></i>}
-                    link="/"
-                    width="w-12"
-                />
-                <PageLink
-                    icon={<i className="fa-light fa-magnifying-glass"></i>}
-                    width="w-12"
-                    onClick={() => setDisplay(!display)}
-                />
-                <PageLink
-                    icon={<i className="fa-light fa-compass"></i>}
-                    link="/explore"
-                    width="w-12"
-                />
-                <PageLink
-                    icon={<i className="fa-light fa-clapperboard-play"></i>}
-                    link="/reels"
-                    width="w-12"
-                />
-                <PageLink
-                    icon={<i className="fa-light fa-paper-plane"></i>}
-                    link="/messages"
-                    width="w-12"
-                />
-                <PageLink
-                    icon={<i className="fa-light fa-heart"></i>}
-                    width="w-12"
-                />
-                <PageLink
-                    icon={<i className="fa-light fa-square-plus"></i>}
-                    width="w-12"
-                />
-                <PageLink
-                    icon={<i className="fa-light fa-user"></i>}
-                    link="/"
-                    width="w-12"
-                />
-            </div>
-            <div>{display ? <SearchPopup /> : ""}</div>
-            <div>{children}</div>
-        </div>
+        <Box display="flex">
+            <Box width={250} display="flex" position={"fixed"}>
+                <Box
+                    display="flex"
+                    flexDirection="column"
+                    height="100vh"
+                    width="72px"
+                    borderRight="1px solid #ddd"
+                    justifyContent="space-between"
+                    zIndex={10}
+                >
+                    <Box>
+                        <Box py={3} textAlign="center">
+                            <Link
+                                to="/"
+                                style={{
+                                    textDecoration: "none",
+                                    fontSize: 24,
+                                    fontWeight: "bold",
+                                }}
+                            >
+                                <i class="fa-brands fa-instagram"></i>
+                            </Link>
+                        </Box>
+
+                        <PageLink
+                            icon={<i className="fa-regular fa-house"></i>}
+                            width="48px"
+                            link="/"
+                        />
+
+                        <PageLink
+                            icon={
+                                <i className="fa-regular fa-magnifying-glass"></i>
+                            }
+                            width="48px"
+                            onClick={handleSearchOpen}
+                        />
+                        <PageLink
+                            icon={<i className="fa-regular fa-compass"></i>}
+                            width="48px"
+                            link="/explore"
+                        />
+                        <PageLink
+                            icon={
+                                <i className="fa-regular fa-clapperboard-play"></i>
+                            }
+                            width="48px"
+                            link="/reels"
+                        />
+                        <PageLink
+                            icon={<i className="fa-regular fa-paper-plane"></i>}
+                            width="48px"
+                            link="/messages"
+                        />
+                        <PageLink
+                            icon={<i className="fa-regular fa-heart"></i>}
+                            width="48px"
+                        />
+                        <PageLink
+                            icon={<i className="fa-regular fa-square-plus"></i>}
+                            width="48px"
+                            onClick={() => setCreateModal(true)}
+                        />
+
+                        {user && (
+                            <PageLink
+                                img={user.profilePicture}
+                                width="48px"
+                                link={`/${user.userName}`}
+                            />
+                        )}
+                    </Box>
+
+                    <Box display="flex" justifyContent="center">
+                        <IconButton onClick={handleMenuOpen}>
+                            <i className="fa-regular fa-bars"></i>
+                        </IconButton>
+                        <Menu
+                            anchorEl={menuAnchor}
+                            open={Boolean(menuAnchor)}
+                            onClose={handleMenuClose}
+                        >
+                            <MenuItem>Item 1</MenuItem>
+                            <MenuItem onClick={handleLogout}>Log out</MenuItem>
+                        </Menu>
+                    </Box>
+                </Box>
+
+                {openSearch && (
+                    <ClickAwayListener onClickAway={handleSearchClose}>
+                        <div>
+                            <SearchPopup />{" "}
+                        </div>
+                    </ClickAwayListener>
+                )}
+            </Box>
+
+            <Box flex={1} marginLeft="72px">
+                {children}
+            </Box>
+
+            {createModal && (
+                <CreatePostModal onClick={() => setCreateModal(false)} />
+            )}
+        </Box>
     );
 }
 
-export default LayoutIcon;
+export default DefaultLayout;
