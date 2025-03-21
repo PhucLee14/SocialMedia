b62123ea13cce97b887546da0ecd7aeb73f4c34e
PhucLee14 <lhphucth14@gmail.com>
2025-03-21 23:21:14 +0700

    update conversation UI

---
+++ frontend/src/components/Message/Note.jsx
@@ -0,0 +1,22 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+function Note({ img, note }) {
+    return (
+        <Box
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                width: "78px",
+            }}
+        >
+            <img src={img} style={{ width: "74px" }} alt="" />
+            <p style={{ fontSize: "12px", color: "#737373" }}>
+                {note ? note : "Your note"}
+            </p>
+        </Box>
+    );
+}
+
+export default Note;
--- frontend/src/pages/Messages.jsx
+++ frontend/src/pages/Messages.jsx
@@ -1,7 +1,80 @@
 import React from "react";
+import { Box } from "@mui/material";
+import Note from "../components/Message/Note";
 
 function Messages() {
-    return <div>Message</div>;
+    const userInfo = JSON.parse(localStorage.getItem("user"));
+    return (
+        <Box sx={{ display: "flex", width: "100vw", height: "100vh" }}>
+            <Box sx={{ width: "400px", borderRight: "1px solid #ccc" }}>
+                <Box sx={{ margin: "40px 20px 20px" }}>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            alignItems: "center",
+                            mb: "40px",
+                        }}
+                    >
+                        <p style={{ fontSize: "20px", fontWeight: "700" }}>
+                            {userInfo.userName}
+                        </p>
+                        <i class="fa-regular fa-pen-to-square fa-xl"></i>
+                    </Box>
+                    <Box
+                    // sx={{
+                    //     display: "flex",
+                    //     flexDirection: "column",
+                    //     // alignItems: "center",
+                    // }}
+                    >
+                        <Note img={userInfo.profilePicture} />
+                    </Box>
+                </Box>
+                <Box></Box>
+            </Box>
+            <Box
+                sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    width: "calc(100% - 400px)",
+                }}
+            >
+                <Box
+                    sx={{
+                        padding: "34px 30px",
+                        border: "2px solid #000",
+                        borderRadius: "50%",
+                    }}
+                >
+                    <i class="fa-brands fa-facebook-messenger fa-2xl"></i>
+                </Box>
+                <Box
+                    sx={{
+                        fontSize: "22px",
+                    }}
+                >
+                    Your messages
+                </Box>
+                <Box sx={{ color: "#737373", mt: "16px" }}>
+                    Send a message to start a chat.
+                </Box>
+                <Box
+                    sx={{
+                        backgroundColor: "#0095f6",
+                        color: "#fff",
+                        padding: "6px 12px",
+                        borderRadius: "10px",
+                        mt: "16px",
+                    }}
+                >
+                    Send message
+                </Box>
+            </Box>
+        </Box>
+    );
 }
 
 export default Messages;
